fb676cdb01a4c8d2fc65b4b1874ff1e104f0e9be
EmZ <35326153+mz462@users.noreply.github.com>
2023-07-11 01:58:12 +0000

    sth

--- newui.py
+++ newui.py
@@ -1,55 +1,48 @@
 import streamlit as st
 from models import Census, OverallCensus, RentalRate, LaborFTE
 
-st.set_page_config(page_title="Assumptions", layout="wide")
+st.sidebar.title("Select Page")
 
-page = st.selectbox("Choose a page", ["Census", "Overall Census", "Rental Rate", "Labor FTE"], key='main')
+page = st.sidebar.selectbox("Choose a page", ["Census", "Overall Census", "Rental Rate", "Labor FTE"])
 
-col1, col2 = st.beta_columns((2,1))
+if page == "Census":
+    st.title("Census Assumptions")
 
-with col1:
-    if page == "Census":
-        st.title("Census Assumptions")
+    available_census_types = ["IL", "AL", "MC"]
+    selected_census_types = st.multiselect("Select Census Types", available_census_types)
 
-        available_census_types = ["IL", "AL", "MC"]
-        selected_census_types = st.multiselect("Select Census Types", available_census_types)
+    censuses = {}
 
-        censuses = {}
+    for census_type in selected_census_types:
+        st.subheader(f"Census Type: {census_type}")
 
-        for census_type in selected_census_types:
-            st.subheader(f"Census Type: {census_type}")
+        start_date = st.date_input(f"{census_type} Start Date")
+        total_units = st.number_input(f"{census_type} Total Units", min_value=0)
+        occupied_units = st.number_input(f"{census_type} Occupied Units", min_value=0, max_value=total_units)
+        occupancy_cap = st.slider(f"{census_type} Occupancy Cap", min_value=0.0, max_value=1.0, step=0.01)
+        second_person_percentage = st.slider(f"{census_type} Second Person Percentage", min_value=0.0, max_value=1.0, step=0.01)
 
-            start_date = st.date_input(f"{census_type} Start Date")
-            total_units = st.number_input(f"{census_type} Total Units", min_value=0)
-            occupied_units = st.number_input(f"{census_type} Occupied Units", min_value=0, max_value=total_units)
-            occupancy_cap = st.slider(f"{census_type} Occupancy Cap", min_value=0.0, max_value=1.0, step=0.01)
-            second_person_percentage = st.slider(f"{census_type} Second Person Percentage", min_value=0.0, max_value=1.0, step=0.01)
+        if st.button(f"Create {census_type} Census"):
+            census = Census(start_date, total_units, occupied_units, occupancy_cap, second_person_percentage, census_type)
+            census.populate_projection()  # Populate the DataFrame
+            censuses[census_type] = census
+            st.write(f"{census_type} Census created successfully.")
 
-            if st.button(f"Create {census_type} Census"):
-                census = Census(start_date, total_units, occupied_units, occupancy_cap, second_person_percentage, census_type)
-                census.populate_projection()  # Populate the DataFrame
-                censuses[census_type] = census
-                st.write(f"{census_type} Census created successfully.")
+        st.write("Created Censuses:", censuses.keys())
 
-            st.write("Created Censuses:", censuses.keys())
+        # Display tables
+        for census_type, census in censuses.items():
+            st.subheader(f"{census_type} Census Projection")
+            st.dataframe(census.get_projection())
 
-            # Display tables
-            for census_type, census in censuses.items():
-                st.subheader(f"{census_type} Census Projection")
-                st.dataframe(census.get_projection())
+elif page == "Overall Census":
+    st.title("Overall Census Page")
+    # Add your code for the Overall Census page here
 
-    elif page == "Overall Census":
-        st.title("Overall Census Page")
-        # Add your code for the Overall Census page here
+elif page == "Rental Rate":
+    st.title("Rental Rate Page")
+    # Add your code for the Rental Rate page here
 
-    elif page == "Rental Rate":
-        st.title("Rental Rate Page")
-        # Add your code for the Rental Rate page here
-
-    elif page == "Labor FTE":
-        st.title("Labor FTE Page")
-        # Add your code for the Labor FTE page here
-
-with col2:
-    st.sidebar.title("Assumptions")
-    # Add your code for the assumptions here
+elif page == "Labor FTE":
+    st.title("Labor FTE Page")
+    # Add your code for the Labor FTE page here
