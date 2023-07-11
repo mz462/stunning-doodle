429bd8ee72a1787dc51a2963db7891891eefd0b4
EmZ <35326153+mz462@users.noreply.github.com>
2023-07-11 01:46:19 +0000

    newui

--- newui.py
+++ newui.py
@@ -3,20 +3,23 @@ from models import Census
 
 st.sidebar.title("Census Assumptions")
 
-census_types = ["IL", "AL", "MC"]
+available_census_types = ["IL", "AL", "MC"]
+selected_census_types = st.sidebar.multiselect("Select Census Types", available_census_types)
+
 censuses = {}
 
-for census_type in census_types:
+for census_type in selected_census_types:
     st.sidebar.subheader(f"Census Type: {census_type}")
 
     start_date = st.sidebar.date_input(f"{census_type} Start Date")
     total_units = st.sidebar.number_input(f"{census_type} Total Units", min_value=0)
     occupied_units = st.sidebar.number_input(f"{census_type} Occupied Units", min_value=0, max_value=total_units)
-    occupancy_cap = st.sidebar.slider(f"{census_type} Occupancy Cap", min_value=0.85, max_value=1.0, step=0.01)
-    second_person_percentage = st.sidebar.slider(f"{census_type} Second Person Percentage", min_value=0.0, max_value=0.5, step=0.01)
+    occupancy_cap = st.sidebar.slider(f"{census_type} Occupancy Cap", min_value=0.0, max_value=1.0, step=0.01)
+    second_person_percentage = st.sidebar.slider(f"{census_type} Second Person Percentage", min_value=0.0, max_value=1.0, step=0.01)
 
     if st.sidebar.button(f"Create {census_type} Census"):
         census = Census(start_date, total_units, occupied_units, occupancy_cap, second_person_percentage, census_type)
+        census.populate_projection()  # Populate the DataFrame
         censuses[census_type] = census
         st.sidebar.write(f"{census_type} Census created successfully.")
 
